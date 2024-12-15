705fb6a43fcd8e8a941f92f863dc8f5ede8fa391
Oussema DHOUIBI <64550550+Thwibi@users.noreply.github.com>
2024-12-15 14:21:52 +0100

    Update HttpRequestUtil.java

--- src/main/java/com/horoscope/HttpRequestUtil.java
+++ src/main/java/com/horoscope/HttpRequestUtil.java
@@ -1,24 +1,23 @@
+package com.horoscope;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpRequestUtil {
-    public static String sendGet(String url, String appId, String appSecret) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("API_ID", appId);
-        con.setRequestProperty("API_SECRET", appSecret);
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
+    public static String sendGet(String url) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
         }
         in.close();
-
         return response.toString();
     }
 }
